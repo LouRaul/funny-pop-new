f4d8a8eb83cb58b9e066c1c3d852acd6ebd74b7b
Raul <loureiroraul@gmail.com>
2020-10-07 08:00:18 -0300

    Template first changes

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "title": "Web Starter",
-  "description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PurgeCSS and PostCSS",
-  "url": "https://webstarter.chriscollins.me",
-  "author": "Chris Collins",
-  "meta_data": {
-    "twitter": "@scottishstoater",
-    "default_social_image": "/static/default_social_image.jpg"
-  },
-  "env": process.env.ELEVENTY_ENV === 'production'
-}
+	title: "Funny Pop",
+	description: "Tienda de pochoclos Gourmet. Eventos y Delivery",
+	url: "https://funnypop.com.ar",
+	author: "Funny Pop",
+	meta_data: {
+		instagram: "@funnypop.ar",
+		default_social_image: "/static/default_social_image.jpg",
+	},
+	env: process.env.ELEVENTY_ENV === "production",
+};
